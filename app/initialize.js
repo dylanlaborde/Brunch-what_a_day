a19c0961988977cab8d92e29cf7d0f7ba3871df0
Dylan <dylan4047@gmail.com>
2017-01-04 09:55:27 +0100

    recuperation du temps avec valeur utilisateur et moment.js

--- app/initialize.js
+++ app/initialize.js
@@ -1,4 +1,53 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
-  console.log('Initialized app');
+
+	let jours = undefined
+	let an = undefined
+	let userdate = undefined
+	let date = undefined
+	let valider = document.getElementById("btCheck")
+	let idDays = document.getElementById("userInputDays")
+	let idYears = document.getElementById("userInputYears")
+	let idMois = document.getElementById("selectMois")
+	let idReset = document.getElementById("restart")
+	let mois = document.getElementById('selectMois').value
+
+
+	let watcher =()=>{
+		valider.addEventListener("click",()=>{
+			// recuperation du temps
+			let userdate = moment([jours,mois,an],"DD-MM-YYYY")
+			let time = userdate.format("dd")
+			let veriftime = userdate.format("llll")
+			console.log(time)
+			console.log(veriftime)
+			console.log("ok")
+			//
+
+		})
+		idDays.addEventListener("change",()=>{
+			jours = document.getElementById("userInputDays").value
+		// console.log(jours)
+		console.log("changement jours")
+	})
+		idYears.addEventListener("change",()=>{
+			an = document.getElementById("userInputYears").value
+			console.log("changement an")
+		})
+		idMois.addEventListener("change",()=>{
+			console.log("changement mois")
+			mois = document.getElementById('selectMois').value
+		// console.log(mois)
+	})
+		idReset.addEventListener("click",()=>{
+			console.log("reset")
+		})
+
+	}
+
+
+	watcher()
+
 });
+
+
+
