95c3d20aee21bb61c35871f865cb5af0d12c24a8
Dylan <dylan4047@gmail.com>
2017-01-04 15:22:41 +0100

    update des condition recuperation des valeur pour moment uniquement si les condition sont respecter ( evite erreur de syntaxe )

--- app/initialize.js
+++ app/initialize.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+	
 	let jours = undefined
 	let an = undefined
 	let userdate = undefined
@@ -9,19 +10,64 @@ document.addEventListener('DOMContentLoaded', () => {
 	let idYears = document.getElementById("userInputYears")
 	let idMois = document.getElementById("selectMois")
 	let idReset = document.getElementById("restart")
-	let mois = document.getElementById('selectMois').value
+	let mois = parseInt(document.getElementById('selectMois').value,10)
 	const hide = "none"
 	const show = "block"
+	let value = undefined
+	let	max = undefined
+	let	mtMax = undefined
+
+
 	document.getElementById("message").style.display = hide
+	let time = undefined
+	
+
+
+
+	
 	let watcher =()=>{
+
 		valider.addEventListener("click",()=>{
+	    mois = parseInt(document.getElementById('selectMois').value,10)
+
+	    // let time = moment().year(an).month(parseInt(mois,10)).date(jours)
+	    // console.log(moment().date(jours).month(mois).year(an).format("llll"))
 			// recuperation du temps
+
+			condition()
+
+		})
+
+		idDays.addEventListener("change",()=>{
+			jours = document.getElementById("userInputDays").value
+
+			console.log("changement jours")
+			
+		})
+
+		idYears.addEventListener("change",()=>{
+			an = document.getElementById("userInputYears").value
+			
+		})
+
+		idMois.addEventListener("change",()=>{
+			
+			mois = document.getElementById('selectMois').value
+
+		})
+		idReset.addEventListener("click",()=>{
+			restart()
+			
+		})
+
+	}
+
+	let check = ()=>{
+
 			let userdate = moment([jours,mois,an],"DD-MM-YYYY")
 			let time = userdate.format("dddd")
 			let veriftime = userdate.format("llll")
-			console.log(time)
 			console.log(veriftime)
-			console.log("ok")
 			// affichage resultat
 			document.getElementById("msgOverlay").innerHTML = time
 			// hide element main
@@ -33,39 +79,19 @@ document.addEventListener('DOMContentLoaded', () => {
 			// resedefinition des couleur des input
 			document.getElementById("userInputDays").style.border = "2px solid #47b8e0"
 			document.getElementById("userInputYears").style.border = "2px solid #47b8e0" 
-			// 
-
-
-
-
-		})
-		idDays.addEventListener("change",()=>{
-			jours = document.getElementById("userInputDays").value
-
-			console.log("changement jours")
-		})
-		idYears.addEventListener("change",()=>{
-			an = document.getElementById("userInputYears").value
-			console.log("changement an")
-		})
-		idMois.addEventListener("change",()=>{
-			console.log("changement mois")
-			mois = document.getElementById('selectMois').value
-
-		})
-		idReset.addEventListener("click",()=>{
-			restart()
-			console.log("reset")
-		})
-
 	}
 
 	let remove = ()=>{
 
 		document.getElementById("msgOverlay").innerHTML = ""
 		document.getElementById("overlay").style.display = hide
-
-		console.log(hide)
+	}
+	let resetvalue = ()=>{
+		jours = undefined
+	an = undefined
+	userdate = undefined
+	date = undefined
+	document.getElementById("userInputYears").value = ""
 	}
 
 	let restart = ()=>{
@@ -73,38 +99,78 @@ document.addEventListener('DOMContentLoaded', () => {
 		document.getElementById("msgOverlay").innerHTML = ""
 		document.getElementById("overlay").style.display = hide
 		document.getElementById("userInputDays").value = ""
-
+		// condition()
+		resetvalue()
 	}
 	let condition = ()=>{
+		userdate = moment([jours,mois,an],"DD-MM-YYYY")
+		time = userdate.format("dddd")
+		console.log(time)
 		
+		if (jours === undefined && an === undefined) {
+			conditionUndefined()
+		}
+		else if (date === "Invalid date") {
+			// coonditionInvalide()
+		}
+		else if (jours > 31) {
+			 conditionWrong()
+		}
+		else {
+			check()
+			console.log("c'est ok ")
+		}
+
+
 	}
 
-
-	let conditionDays = ()=>{
-		document.getElementById("userInputDays").style.border = "2px solid #ff7473" 
-		document.getElementById("main").style.display = show 
-		document.getElementById("overlay").style.display = hide
-		document.getElementById("msgOverlay").innerHTML = ""
-
-
-
-	}
-	let conditionYears = ()=>{
-		document.getElementById("userInputYears").style.border = "2px solid #ff7473" 
-		document.getElementById("main").style.display = show 
-		document.getElementById("overlay").style.display = hide
-		document.getElementById("msgOverlay").innerHTML = ""
+	let conditionUndefined = ()=>{
+		document.getElementById("userInputDays").style.border = "2px solid #ff7473"
+		document.getElementById("userInputYears").style.border = "2px solid #ff7473"
+		document.getElementById("message").style.display = show
 		document.getElementById("msg").innerHTML = ""
-		document.getElementById("message").style.display = show 
-		document.getElementById("msg").innerHTML = "Renseigner l'année !"
+		document.getElementById("msg").innerHTML = "Erreur aucune valeur n'a été renseigné"
+		document.getElementById("main").style.display = show
+		document.getElementById("overlay").style.display = hide
+
+	}
+	
+	let conditionWrong = ()=>{
+		document.getElementById("userInputDays").style.border = "2px solid #ff7473"
+		document.getElementById("message").style.display = show
+		document.getElementById("msg").innerHTML = ""
+		document.getElementById("msg").innerHTML = "Le jour doit etre compris en 1 et 31 "
+		document.getElementById("main").style.display = show
+		document.getElementById("overlay").style.display = hide
+	}
+	
+	// let conditionDays = ()=>{
+	// 	document.getElementById("userInputDays").style.border = "2px solid #ff7473" 
+	// 	document.getElementById("main").style.display = show 
+	// 	document.getElementById("overlay").style.display = hide
+	// 	document.getElementById("msgOverlay").innerHTML = ""
+
+
+
+	// }
+	// let conditionYears = ()=>{
+	// 	document.getElementById("userInputYears").style.border = "2px solid #ff7473" 
+	// 	document.getElementById("main").style.display = show 
+	// 	document.getElementById("overlay").style.display = hide
+	// 	document.getElementById("msgOverlay").innerHTML = ""
+	// 	document.getElementById("msg").innerHTML = ""
+	// 	document.getElementById("message").style.display = show 
+	// 	document.getElementById("msg").innerHTML = "Renseigner l'année !"
 
 		
-	}
-
+	// }
+	
 	remove()
 	watcher()
+// console.log(moment().year(2012).month(11).date(21).format("ll")) 
 
 });
+
 
 
 
